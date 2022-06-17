3d3661e64a95f5c0bf6a42983bbb8858b085aa39
Hossam Khair <hossam.khir1@outlook.com>
2022-06-17 23:35:13 +0200

    feat: finished intro to python

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,8 @@ def primary():
 
     last = len(quotes) - 1
     rnd = random.randint(0, last)
-    print(quotes[rnd])
+    # print(quotes[rnd])
+    print("\n".join(map(str.strip, quotes[0:rnd])))
 
 
 if __name__ == "__main__":
